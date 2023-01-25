44dd2aa6f9b22ba16f726399b73dc7859dd6db33
huyhoanFithcmus <huyhoan652002@gmail.com>
2023-01-25 20:46:26 +0700

    add Category.js

--- CourseSellerExpo/App.js
+++ CourseSellerExpo/App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 import Category from '../CourseSellerExpo/components/Category';
 
 export default function App() {
+  const categories = [
+    { id: 1, name: "Spike Prime" },
+    { id: 2, name: "Scratch" },
+    { id: 3, name: "Mindstorms" },
+    { id: 4, name: "Wedo" },
+    { id: 5, name: "EV3" },
+    { id: 6, name: "Arduino" },
+    { id: 7, name: "Python" },
+  ];
+
   return (
     <View style={styles.container}>
-      <Category title="Spike Prime" />
-      <Category title="MindStorms" />
-      <Category title="Arduino" />
-      <Category title="Python" />
+      <ScrollView>
+        {categories.map((category) => (
+          <Category key={category.id} title={category.name} />
+        ))}
+        {/* <FlatList data={categories} renderItem={({ item }) => <Category title={item.name} />} keyExtractor={(item) => item.id.toString()} /> */}
+      </ScrollView>
     </View>
     
   );
